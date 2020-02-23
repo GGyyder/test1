8f86919497991cb29829284f2d3644dbe288ae53
a7603307 <a7603307@gmail.com>
2020-02-24 00:10:20 +0800

    1111

--- TEST/Controllers/EmployeesController.cs
+++ TEST/Controllers/EmployeesController.cs
@@ -11,10 +11,13 @@ namespace TEST.Controllers
     public class EmployeesController : Controller
     {
         // GET: Employees
-        public ActionResult EmployeeList()
+        public ActionResult EmployeeList(string message="")
         {
-         
 
+            if (!string.IsNullOrEmpty(message))
+            {
+                ViewBag.message = message;
+            }
 
                 return View();
         }
@@ -24,15 +27,22 @@ namespace TEST.Controllers
 
             List<Employees> employeesList = new List<Employees>();
             int pagecount = 10;
-            int skipcount = (page - 1) * 10;
+            int skipcount = (page - 1) * pagecount;
+            int Total = 1;
             using (NorthwindEntities db = new NorthwindEntities())
             {
 
                 employeesList = db.Employees.OrderBy(c=>c.EmployeeID).Skip(skipcount).Take(pagecount).ToList();
-
+                Total = db.Employees.Count();
             }
 
-
+            int Totalpage = 1;
+            if (Total > 10)
+            {
+                
+             Totalpage=(Total / 10) + 1;
+            }
+            ViewBag.Totalpage = Totalpage;
             return View(employeesList);
         }
 
@@ -44,7 +54,8 @@ namespace TEST.Controllers
             {
 
                 employee = db.Employees.Find(EmployeeID);
-
+                var query = db.Employees.ToList();
+                ViewBag.Sales = new SelectList(query, "EmployeeID", "LastName");
             }
             
 
@@ -52,6 +63,59 @@ namespace TEST.Controllers
             return View(employee);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpEmployee(Employees employees, HttpPostedFileBase newphoto)
+        {
+            try
+            {
+
+
+                Employees exmeployees = new Employees();
+
+                if (newphoto != null)
+                {
+                    string contentype = newphoto.ContentType.Replace("image/", "");
+                    string up_contentype = contentype.ToUpper();
+                    if (up_contentype != "JPEG" && up_contentype != "GIF" && up_contentype != "PNG") //上傳檔案類型的驗證
+                    {
+
+                        return RedirectToAction("EmployeeList", new { message = "上傳圖檔格式不符 請重新上傳" });
+                    }
+                }
+                
+
+                if (!ModelState.IsValid)
+                {
+
+                    return RedirectToAction("EmployeeList", new { message = "修改失敗" });
+                }
+                else
+                {
+
+
+                    if (newphoto != null)
+                    {
+                        using (var binaryReader = new BinaryReader(newphoto.InputStream))
+                        {
+                            employees.Photo = binaryReader.ReadBytes(newphoto.ContentLength);
+                        }
+                    }
+                    using (NorthwindEntities db = new NorthwindEntities())
+                    {
+
+                        db.Entry(employees).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                    return RedirectToAction("EmployeeList", new { message = "修改成功" });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return RedirectToAction("EmployeeList", new { message = "修改失敗" });
+            }
+        }
      
         public ActionResult EmployeeDetail(int EmployeeID)
         {
@@ -68,6 +132,8 @@ namespace TEST.Controllers
 
             return View(employee);
         }
+
+   
         public ActionResult DeleteEmployee (int EmployeeID)
         {
             Employees employee = new Employees();
@@ -94,7 +160,15 @@ namespace TEST.Controllers
 
         public ActionResult AddEmployeeDetail()
         {
-          
+
+            using (NorthwindEntities db = new NorthwindEntities())
+            {
+
+         
+           var query = db.Employees.ToList();
+            ViewBag.Sales = new SelectList(query, "EmployeeID", "LastName");
+            }
+           
 
 
 
@@ -107,15 +181,23 @@ namespace TEST.Controllers
 
             try
             {
+
+
+
+
                 if (newphoto != null)
                 {
                     string contentype = newphoto.ContentType.Replace("image/", "");
                     string up_contentype = contentype.ToUpper();
                     if (up_contentype != "JPEG" && up_contentype != "GIF" && up_contentype != "PNG") //上傳檔案類型的驗證
                     {
-           
+
                         return RedirectToAction("EmployeeList", new { message = "上傳圖檔格式不符 請重新上傳" });
                     }
+                }
+                else
+                {
+                    return RedirectToAction("EmployeeList", new { message = "上傳圖檔格式不符 請重新上傳" });
                 }
                
                 if (!ModelState.IsValid)
@@ -138,7 +220,7 @@ namespace TEST.Controllers
                     {
 
                         db.Employees.Add(employees);
-
+                      
                         db.SaveChanges();
                     }
                     return RedirectToAction("EmployeeList", new { message = "加入成功" });
